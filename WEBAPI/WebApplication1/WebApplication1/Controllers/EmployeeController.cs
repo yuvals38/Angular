48966403e2fac6f3280d31fda02c53e6bc4193e2
yuvals38 <58080611+yuvals38@users.noreply.github.com>
2020-03-03 13:09:55 +0200

    Angular+webAPI+CRUD ops
    Angular using webAPI (custom) with CRUD operations

---
+++ WEBAPI/WebApplication1/WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplication1.Models;
+using System.Web.Http.Cors;
+
+namespace WebApplication1.Controllers
+{
+    // [EnableCors(origins: "http://localhost:44374", headers:"*", methods:"*")]
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
+    public class EmployeeController : ApiController
+    {
+        private DBModel db = new DBModel();
+
+        // GET: api/Employee
+        public IQueryable<Employee> GetEmployee()
+        {
+            return db.Employee;
+        }
+
+        // PUT: api/Employee/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutEmployee(int id, Employee employee)
+        {
+        
+            if (id != employee.EmployeeID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Employee
+        [ResponseType(typeof(Employee))]
+        public IHttpActionResult PostEmployee(Employee employee)
+        {
+           
+            db.Employee.Add(employee);
+            db.SaveChanges();
+
+            //not necessarily in use with webapi and angular?
+            return CreatedAtRoute("DefaultApi", new { id = employee.EmployeeID }, employee);
+        }
+
+        // DELETE: api/Employee/5
+        [ResponseType(typeof(Employee))]
+        public IHttpActionResult DeleteEmployee(int id)
+        {
+            Employee employee = db.Employee.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            db.Employee.Remove(employee);
+            db.SaveChanges();
+
+            return Ok(employee);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return db.Employee.Count(e => e.EmployeeID == id) > 0;
+        }
+    }
+}
